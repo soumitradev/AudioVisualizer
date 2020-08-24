ceb9d7bac2934da4835c7f164bd0adc3df971622
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-08-24 17:27:01 +0530

    :tada: Finish rebinning, smoothing, interpolation, and pygame

---
+++ AudioVisualizer.py
@@ -0,0 +1,181 @@
+# Import dependencies
+import numpy as np
+import math
+from scipy.io import wavfile
+import time
+import pygame
+import sys
+
+# Define some constants that will be used throughout the script
+SONG_FILE = './tst.wav'
+NUMBER_OF_PACKETS_PER_SEC = 15
+NUMBER_OF_BINS = 50
+BLOCK_SIZE = 20
+PADDING = 5
+
+# Color constants
+CELL_COLOR = 255, 255, 255
+BG_COLOR = 0, 0, 0
+
+# Pygame window size
+size = width, height = PADDING + NUMBER_OF_BINS*(BLOCK_SIZE + PADDING), 680
+
+# Define an FFT function that we'll use for analysing audio
+
+# Note how we also pass the number of samples as an argument
+# since we call this function a lot and repeatedly calculating number of samples
+# (which is a constant) will only slow it down
+def FFT(stuff, sampling_rate, num_samples):
+    # Perform an FFT on our audio data and grab the first half
+    # since the second half is a mirror of the first in a fourier transform
+    F = list(np.fft.fft(stuff))[:round(num_samples/2)]
+
+    # Calculate sampling resolution
+    sampling_resolution = sampling_rate/num_samples
+
+    # For every frquency we get, double the data and get the magnitude of the vector,
+    # and divide by number of samples to get actual amplitude
+    return [(2*abs(freq)/num_samples, i*sampling_resolution) for i, freq in enumerate(F)]
+
+    # Notice how we used a list comprehension (which is faster than a for loop) and enumerate()
+    # instead of accessing F[i] repeatedly. We are trading memory space for speed.
+    # However, note that this won't take a whole lot of memory since we work with only around 
+    # 2000 sized lists with pure numbers in it, and that all this memory space is deallocated
+    # after the function returns.
+
+# Write a function to get the size of the overlap between two ranges. We will use this for rebinning
+def get_overlap(t1, t2):
+    x = min(t1[1], t2[1]) - max(t1[0], t2[0])
+    return x if x > 0 else 0
+
+# Read in the song file
+sample_rate, data = wavfile.read(SONG_FILE)
+
+
+a = time.time()
+# Check if audio file is stereo. If it is, convert it to mono
+mono = False
+if type(data[0]) == np.ndarray:
+    mono = True
+    print("Stereo Audio file detected, converting to Mono. The original file will NOT be modified.")
+    data = [sum(k)/2 for k in data]
+    print("Conversion to Mono completed.")
+
+# Set some constants that will be used
+N = len(data)
+packet_len = int(sample_rate/NUMBER_OF_PACKETS_PER_SEC)
+
+print("Processing audio...")
+e = time.time()
+
+# Break audio samples into packets, and perform FFT on each packet
+packeted_data = [data[i*packet_len:(i+1)*packet_len] for i in range(N//packet_len + 1)]
+fft_ed_data = [FFT(packet, sample_rate, packet_len) for packet in packeted_data]
+print("FFT completed.")
+d = time.time()
+
+
+rebinned = []
+
+# Calculate new bin limits, and rebin each packet
+for packet in fft_ed_data:
+    amps, old_bins = zip(*packet)
+    old_bins = list(old_bins)
+    old_bins.append(sample_rate/2)
+    bin_limits = [sample_rate/2 * (B_i/NUMBER_OF_BINS)**2 for B_i in range(NUMBER_OF_BINS + 1)]
+
+    new_bin_vals = []
+
+    for i in range(1, NUMBER_OF_BINS + 1):
+        # Some initial variables for rebinning
+        low = 0
+        l_lim = bin_limits[i-1]
+        hi = 0
+        h_lim = bin_limits[i]
+
+        # Grab the intervals in old bins that become part of the new bins
+        for j in range(hi, int(sample_rate/NUMBER_OF_PACKETS_PER_SEC + 1)):
+            if old_bins[j - 1] <= l_lim:
+                low = j
+            if old_bins[j] >= h_lim:
+                hi = j
+                break
+        # Initialize a sum and add the corresponding portion of the audio intensity to our new bin.
+        s = 0
+        for k in range(low, hi+1):
+            s += packet[min(k, 1469)][0] * (get_overlap((bin_limits[i-1], bin_limits[i]), (old_bins[max(0, k-1)], old_bins[min(k, 1469)])) / (old_bins[k] - old_bins[max(0, k-1)]))
+        
+        # Append the rebinned bin value to our new bin value list
+        new_bin_vals.append(s)
+    # Append packet to the rebinned binned list
+    rebinned.append(new_bin_vals)
+
+print("Rebinning completed.")
+c = time.time()
+
+# Interpolate data between packets and adjust the intensity of sound according to our smoothing function
+interpolated = []
+glob_max = max([max(packet) for packet in rebinned])
+for i in range(len(rebinned) - 1):
+    interpolated.append([math.log((m/glob_max)**2 + 1)/math.log(2) for m in rebinned[i]])
+    interpolated_packet = [(rebinned[i][j] + rebinned[i+1][j])/2 for j in range(NUMBER_OF_BINS)]
+    interpolated.append([math.log((m/glob_max)**2 + 1)/math.log(2) for m in interpolated_packet])
+
+
+print("Interpolation completed.")
+b = time.time()
+
+# Print some timing information
+print(f'Total: {b-a}, Interpolation: {b-c}, Binning: {c-d}, FFT: {d-e}, Mono Conversion: {0 if not mono else e-a}')
+
+# Set FPS (slightly more than 30 so that the delay in rendering the bars is accounted for)
+FPS = 32
+
+# Initialise PyGame, and set some constants
+pygame.init()
+
+# Initialize audio
+mixer = pygame.mixer
+mixer.init() 
+  
+# Loading the song for pygame to play
+mixer.music.load(SONG_FILE) 
+  
+# Setting the volume 
+mixer.music.set_volume(0.7) 
+  
+# Start playing the song 
+# Create screen for showing viusalisation
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Audio Visualiser")
+
+# Keep track of which packet is being rendered on screen
+cur_packet_index = 0
+
+# While game is running, for every frame,
+mixer.music.play()
+while cur_packet_index < len(interpolated):
+
+    # Store events to process quit and KeyDown events
+    events = pygame.event.get()
+
+    # Handle exit event
+    for event in events:
+        # Let user exit
+        if event.type == pygame.QUIT: sys.exit()            
+
+    # Fill the screen with BG_COLOR
+    screen.fill(BG_COLOR)
+
+    # Show every bar
+    for ind, intensity in enumerate(interpolated[cur_packet_index]):
+        pygame.draw.rect(screen, CELL_COLOR, pygame.Rect(PADDING + ind*(PADDING + BLOCK_SIZE), 690, BLOCK_SIZE, -int(intensity * 660)))
+
+
+    # Update the display
+    pygame.display.update()
+
+    # Tick the display as per FPS, and move to next packet
+    pygame.time.Clock().tick(FPS)
+    cur_packet_index += 1
+    
--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-# Audio Visualizer
-This is an old project (around 2 weeks old) in which I used FFTs to create a simple audiovisualizer. It reads in a music file (.wav format) and spits out a .mp4 file with a graph of the frequencies. A sample .mp4 file is in the repo. To run this, make sure to replace tst.wav everywhere in your code with your wav file, and make sure the ffmpeg command at the end spits out the video file name you want.
+# :musical_note: Audio Visualizer
+This is a python script that visualizes music files. (Currently only limited to 16 and 32 bit PCM encoded .wav files). I know that sounds very specific, but don't worry! Your audio file when in wav format is probably already compatible since this is one of the most popular formats.
 
- I might document and rewrite for live audio using pygame later.
+Stereo audio files are converted to mono for processing. This takes more time to analyze audio, but the original audio file is not modified.
 
-<!-- The final pygame implementation is in `audio_visualiser.py` -->
+Read the [Jupyter notebook](./Explanation.ipynb) for more information.
 
-Read the Jupyter Notebook for more details on how I thought this out and how this works.
+## Why?
+Some day in May 2020, I had too much coffee, and sat down to learn how JPEG compression works (because I just got finished watching Silcon Valley, and my interest in compression peaked). I learnt about the DCT, and slowly as I went down the rabbit hole, I came out the other side of the coin: I was learning how DFTs can be used for wave analysis.
+
+It had to be done. I had to write an audio visualizer.
+
+So I sat down, read up about DFTs, and tried a small example of an FFT with numpy in a Jupyter Notebook. I used matplotlib to sketch the graphs, and ffmpeg to stitch the frames for the video together. But, I hadn't written any documentation or comments. I was happy with analysing one file, and I uploaded it to GitHub.
+
+Later in August, someone asked me about the most fun project I've worked on. My mind immediately went to this project. However, I couldn't really show or explain it since it was just a wall of text with badly named variables. So, I decided to document it, and optimize parts of it.
+
+And now, here it is. It is complete with a mathematical explanation of how the audio is analyzed.
+
+Easily one of my proudest projects.
+
+## Usage
+The complete explanation of how this works is in the [Jupyter notebook](./Explanation.ipynb). I'd suggest reading that before going through the [main script](./AudioVisualizer.py).
+
+The script is a python script that requires numpy, scipy, and pygame. These can be installed by running `pip install -r requirements.txt`. After that, you can can edit the `AudioVisualizer.py` to load your music file instead of the test one I've included.
+
+:warning: **The test file I've included (tst.wav) is a full pan of the 0-22kHz audio spectrum. It contains very irritating sounds.**
+
+To run the visualizer, run `python ./AudioVisualizer.py`
+
+The script is fairly slow, especially the rebinning process. I might optimize this in the future, and may make rebinning optional.
+
+The script is slow only in 2 parts:
+- Converting stereo to mono, and
+- Rebinning the data we get after performing the FFT.
+
+If you decide to feed it a mono file, it will be much faster since the script need not convert it in memory.
+
+On a test audio file, I got the following timings in seconds for each step:
+- **Mono file:** Total: 38.949s, Interpolation: 0.396s, Binning: 34.263s, FFT: 4.290s, Mono Conversion: 0
+- **Stereo file:** Total: 62.783s, Interpolation: 0.392s, Binning: 31.888s, FFT: 4.96s, Mono Conversion: 25.539s
+
+## Contribution
+*Any* help with optimization is appreciated. Other than that, more audio formats, and miscellaneous features are also welcome. Please create an Issue, and if possible, a Pull Request for new/suggested features.
